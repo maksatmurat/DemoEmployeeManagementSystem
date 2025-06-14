2115771b05d9b7b91195c2e36ea80a6ecc1bf8f0
maksatmurat <maksatmyrat.muhammetorazov@ogr.sakarya.edu.tr>
2025-06-14 15:27:33 +0500

    Go back 2

---
+++ ServerLibrary/Repositories/Implementations/BranchRepository.cs
@@ -0,0 +1,52 @@
+﻿using BaseLibraty.Entities;
+using BaseLibraty.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations;
+
+public class BranchRepository(AppDbContext appDbContext) : IGenericRepositoryInterface<Branch>
+{
+    public async Task<GeneralResponse> DeleteById(int id)
+    {
+        var brc = await appDbContext.Branches.FindAsync(id);
+        if (brc is null) return NotFound();
+
+        appDbContext.Branches.Remove(brc);
+        return await Commit().ContinueWith(_ => Success());
+    }
+    public async Task<List<Branch>> GetAll() => await appDbContext.Branches.ToListAsync();
+
+    public async Task<Branch> GetById(int id)
+    {
+        var entity = await appDbContext.Branches.FindAsync(id);
+        return entity is null ? throw new InvalidOperationException($"Branch with ID {id} not found.") : entity;
+    }
+
+    public async Task<GeneralResponse> Insert(Branch item)
+    {
+        if (!await CheckName(item.Name!)) return new GeneralResponse(false, "Branch already added");
+        appDbContext.Branches.Add(item);
+        return await Commit().ContinueWith(_ => Success());
+    }
+
+
+
+    public async Task<GeneralResponse> Update(Branch item)
+    {
+        var brc = await appDbContext.Branches.FindAsync(item.Id);
+        if (brc is null) return NotFound();
+        brc.Name = item.Name;
+        return await Commit().ContinueWith(_ => Success());
+    }
+
+    private async Task Commit() => await appDbContext.SaveChangesAsync();
+    private static GeneralResponse NotFound() => new(false, "Sorry Branch not fount");
+    private static GeneralResponse Success() => new(true, "Process completed");
+    private async Task<bool> CheckName(string name)
+    {
+        var item = await appDbContext.Branches.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
+        return item is null;
+    }
+}
---
+++ ServerLibrary/Repositories/Implementations/DepartmentRepository.cs
@@ -0,0 +1,52 @@
+﻿using BaseLibraty.Entities;
+using BaseLibraty.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+using System;
+
+namespace ServerLibrary.Repositories.Implementations;
+public class DepartmentRepository(AppDbContext appDbContext) : IGenericRepositoryInterface<Department>
+{
+    public async Task<GeneralResponse> DeleteById(int id)
+    {
+        var dep = await appDbContext.Departments.FindAsync(id);
+        if (dep is null) return NotFound();
+
+        appDbContext.Departments.Remove(dep);
+        return await Commit().ContinueWith(_ => Success());
+    }
+
+    public async Task<List<Department>> GetAll() => await appDbContext.Departments.ToListAsync();
+
+    public async Task<Department> GetById(int id)
+    {
+        var entity = await appDbContext.Departments.FindAsync(id);
+        return entity is null ? throw new InvalidOperationException($"Department with ID {id} not found.") : entity;
+    }
+
+    public async Task<GeneralResponse> Insert(Department item)
+    {
+        if (!await CheckName(item.Name!)) return new GeneralResponse(false, "Department already added");
+        appDbContext.Departments.Add(item);
+        return await Commit().ContinueWith(_ => Success());
+   }
+
+
+
+    public async Task<GeneralResponse> Update(Department item)
+    {
+        var dep = await appDbContext.Departments.FindAsync(item.Id);
+        if (dep is null) return NotFound();
+        dep.Name = item.Name;
+        return await Commit().ContinueWith(_ => Success());
+    }
+    private async Task Commit() => await appDbContext.SaveChangesAsync();
+    private static GeneralResponse NotFound() => new(false, "Sorry department not fount");
+    private static GeneralResponse Success() => new(true, "Process completed");
+    private async Task<bool> CheckName(string name)
+    {
+        var item = await appDbContext.Departments.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
+        return item is null;
+    }
+}
