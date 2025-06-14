e23473015447b960a2910d1fe34e8bb4ee8bb181
maksatmurat <maksatmyrat.muhammetorazov@ogr.sakarya.edu.tr>
2025-06-14 15:27:12 +0500

    Go back

--- ServerLibrary/Data/AppDbContext.cs
+++ ServerLibrary/Data/AppDbContext.cs
@@ -10,6 +10,13 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<Department> Departments { get; set; }
     public DbSet<Branch> Branches { get; set; }
     public DbSet<Town> Towns { get; set; }
+<<<<<<< Updated upstream
+=======
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<City> Cities { get; set; }
+
+    //Authentication /Role /System Roles 
+>>>>>>> Stashed changes
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<SystemRole> SystemRoles { get; set; }
     public DbSet<UserRole> UserRoles { get; set; }
---
+++ ServerLibrary/Repositories/Implementations/GeneralDepartmentRepository.cs
@@ -0,0 +1,54 @@
+﻿using BaseLibraty.Entities;
+using BaseLibraty.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations;
+
+public class GeneralDepartmentRepository(AppDbContext appDbContext) : IGenericRepositoryInterface<GeneralDepartment>
+{
+
+    public async Task<GeneralResponse> DeleteById(int id)
+    {
+        var dep= await appDbContext.GeneralDepartments.FindAsync(id);
+        if (dep is null) return NotFound();
+
+        appDbContext.GeneralDepartments.Remove(dep);
+        return await Commit().ContinueWith(_ => Success());
+    }
+
+    public async Task<List<GeneralDepartment>> GetAll() => await appDbContext.GeneralDepartments.ToListAsync();
+
+    public async Task<GeneralDepartment> GetById(int id)
+    {
+        var entity = await appDbContext.GeneralDepartments.FindAsync(id);
+        return entity is null ? throw new InvalidOperationException($"GeneralDepartment with ID {id} not found.") : entity;
+    }
+
+    public async Task<GeneralResponse> Insert(GeneralDepartment item)
+    {
+        if (!await CheckName(item.Name!)) return new GeneralResponse(false, "Department already added");
+        appDbContext.GeneralDepartments.Add(item);
+        return await Commit().ContinueWith(_ => Success());
+    }
+
+   
+
+    public async Task<GeneralResponse> Update(GeneralDepartment item)
+    {
+        var dep = await appDbContext.GeneralDepartments.FindAsync(item.Id);
+        if (dep is null) return NotFound();
+        dep.Name = item.Name;
+        return await Commit().ContinueWith(_ => Success());
+    }
+
+    private static GeneralResponse NotFound() => new(false, "Sorry department not fount");
+    private static GeneralResponse Success() => new(true, "Process completed");
+    private async Task Commit()=>await appDbContext.SaveChangesAsync();
+    private async Task<bool> CheckName(string name)
+    {
+        var item = await appDbContext.Departments.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
+        return item is null;
+    }
+}
